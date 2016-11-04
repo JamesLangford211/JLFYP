289dbf35ce3abefaa715a087d0efa2c06b496706
James Langford <James@100.71.40.9>
2016-11-04 04:20:54 +0000

    Added Jdoc to Edge class, just to test a second commit mainly.

--- finalYearProject/src/finalYearProject/Edge.java
+++ finalYearProject/src/finalYearProject/Edge.java
@@ -1,5 +1,14 @@
 package finalYearProject;
 
+/**
+* Class to represent the connection between two nodes in a flowchart.
+* 
+*
+* @author  James Langford
+* @version 1.0
+* @since   02-11-2016 
+*/
+
 public class Edge {
 	public Node source;
 	public Node destination;
